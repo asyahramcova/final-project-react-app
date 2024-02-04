62ebd91bb7a66ae745ab1f195efda78cc28aa770
asyahramcova <asyahramcova@gmail.com>
2024-02-05 00:18:21 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ function App() {
     <div className="App">
     <Router>
       <Navbar/>
+      <Home/>
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/about" element={<About/>} />
